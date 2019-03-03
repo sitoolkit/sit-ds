b57420fcc7778a094708c208c248d9bfbd52d2c9
Yuichi Kuwhara <yuichi.kuwahara.0@gmail.com>
2019-03-03 16:38:22 +0900

    Make init.sh rerunnable

--- service/work/mount/init.sh
+++ service/work/mount/init.sh
@@ -4,6 +4,6 @@ set -eux
 
 dockerize -wait tcp://ldap:${LDAP_PORT} -wait tcp://its:${ITS_PORT} 
 
-ldapadd -h ldap -x -D "cn=admin,dc=example,dc=org" -w admin -f /tmp/add-users.ldif
+ldapadd -c -h ldap -x -D "cn=admin,dc=example,dc=org" -w admin -f /tmp/add-users.ldif || true
 
-psql -h dbms -d redmine -U redmine -f /tmp/redmine-additional-config.sql
+psql -h dbms -d redmine -U redmine -f /tmp/redmine-additional-config.sql || true
--- service/work/mount/redmine-additional-config.sql
+++ service/work/mount/redmine-additional-config.sql
@@ -3,13 +3,13 @@ INSERT INTO auth_sources
 VALUES(1, 'AuthSourceLdap', 'LDAP', '{{ .Env.LDAP_HOST  }}', {{ .Env.LDAP_PORT }}, '{{ .Env.LDAP_MANAGER_DN  }}', '{{ .Env.LDAP_MANAGER_PASSWORD  }}', '{{ .Env.LDAP_ROOT_DN  }}', '{{ .Env.LDAP_LOGIN_ATTRIBUTE }}', 'givenName', 'sn', '{{ .Env.LDAP_MAIL_ATTRIBUTE }}', true, false, '', NULL);
 
 INSERT INTO settings
-  (name, value, updated_on)
+  (id, name, value, updated_on)
 VALUES
-  ('emails_footer', 'You have received this notification because you have either subscribed to it, or are involved in it.\r\nTo change your notification preferences, please click here: {{ .Env.PUBLIC_PROTOCOL }}://{{ .Env.PUBLIC_HOST }}/redmine/my/account', current_timestamp)
-  ,('host_name', '{{ .Env.PUBLIC_HOST }}/redmine', current_timestamp)
-  ,('mail_from', '{{ .Env.MAIL_RELAY_USER }}', current_timestamp)
-  ,('protocol', '{{ .Env.PUBLIC_PROTOCOL }}', current_timestamp)
-  ,('text_formatting', 'markdown', current_timestamp)
+  (1, 'emails_footer', 'You have received this notification because you have either subscribed to it, or are involved in it.\r\nTo change your notification preferences, please click here: {{ .Env.PUBLIC_PROTOCOL }}://{{ .Env.PUBLIC_HOST }}/redmine/my/account', current_timestamp)
+  ,(2, 'host_name', '{{ .Env.PUBLIC_HOST }}/redmine', current_timestamp)
+  ,(3, 'mail_from', '{{ .Env.MAIL_RELAY_USER }}', current_timestamp)
+  ,(4, 'protocol', '{{ .Env.PUBLIC_PROTOCOL }}', current_timestamp)
+  ,(5, 'text_formatting', 'markdown', current_timestamp)
   ;
 
 SELECT *
