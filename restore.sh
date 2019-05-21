45b42a0fd08d48d62504a4ab7528e33ca52ea4a5
mortosh1003 <mortosh1003@gmail.com>
2019-05-21 10:37:11 +0900

    Modify restore.sh

--- restore.sh
+++ restore.sh
@@ -3,13 +3,15 @@
 # This script is to restore docker volumes defined in sit-ds/docker-compose.yml.
  
 if [ $# = 0 ]; then
-  log 'Retry add the full path of the backup directory to the argument.'
+  echo 'Retry add the full path of the backup directory to the argument.'
   exit
 else
   readonly BACKUP_DIR=$1
 fi
 
 do_restore() {
+  readonly VOLUME_PREFIX='sit-ds_'
+  
   for restore_file in $(ls ${BACKUP_DIR}); do
     if [ $(echo $restore_file | grep -e "${VOLUME_PREFIX}") ] && [ $(echo $restore_file | grep -e ".tar") ]; then
       volume_name=${restore_file%.*}
