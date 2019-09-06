77116de3550192e4e2d21da853f0851a7c5041b9
kfujinuma <40159739+kfujinuma@users.noreply.github.com>
2019-09-06 12:12:53 +0900

    Feature/redmine ldap (#18)

--- service/work/mount/init.sh
+++ service/work/mount/init.sh
@@ -4,7 +4,7 @@ set -eux
 
 readonly INIT_FILE='/tmp/.init'
 
-dockerize -wait tcp://ldap:${LDAP_PORT} -wait tcp://its:${ITS_PORT} 
+dockerize -wait tcp://ldap:${LDAP_PORT} -wait tcp://its:${ITS_PORT} -timeout 60s
 
 if [[ -e ${INIT_FILE} ]]; then
 
