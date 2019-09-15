7af7c16b8d561f5e6745da4a0eb56f769bcaacfd
kumakuratk <kumagera0709@gmail.com>
2019-09-15 18:19:05 +0900

    Modify to archive backup file (#23)

--- restore.sh
+++ restore.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # This script is to restore docker volumes defined in sit-ds/docker-compose.yml.
- 
+
 if [ $# = 0 ]; then
   echo 'Retry add the full path of the backup directory to the argument.'
   exit
@@ -11,16 +11,16 @@ fi
 
 do_restore() {
   readonly VOLUME_PREFIX='sit-ds_'
-  
+
   for restore_file in $(ls ${BACKUP_DIR}); do
-    if [ $(echo $restore_file | grep -e "${VOLUME_PREFIX}") ] && [ $(echo $restore_file | grep -e ".tar") ]; then
-      volume_name=${restore_file%.*}
+    if [ $(echo $restore_file | grep -e "${VOLUME_PREFIX}") ] && [ $(echo $restore_file | grep -e ".tar.gz") ]; then
+      volume_name=${restore_file%%.*}
       echo "Start restore ${volume_name}."
 
       docker run --rm \
         -v ${volume_name}:/target \
         -v ${BACKUP_DIR}:/backup \
-        ubuntu bash -c 'tar xvf /backup/'${restore_file}' -C /target'
+        ubuntu bash -c 'tar xfz /backup/'${restore_file}' -C /target'
     fi
   done
 
